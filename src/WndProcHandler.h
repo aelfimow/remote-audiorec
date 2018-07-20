1a61b2de6e642e165227c35f5260779aaab96981
Alexander Elfimow <my.free.mail@gmx.de>
2018-07-20 21:52:48 +0200

    Changed interface

--- src/WndProcHandler.h
+++ src/WndProcHandler.h
@@ -3,13 +3,15 @@
 #else
 #define WND_PROC_HANDLER_H
 
+class WndProcParam;
+
 class WndProcHandler
 {
     public:
         WndProcHandler() { }
         virtual ~WndProcHandler() { }
 
-        virtual LRESULT operator()(HWND hwnd, WPARAM wParam, LPARAM lParam) = 0;
+        virtual LRESULT operator()(const WndProcParam &param) = 0;
 
     public:
         WndProcHandler(const WndProcHandler &instance) = delete;
